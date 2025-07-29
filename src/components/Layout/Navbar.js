45e231fa12de3dabe6727b23e49129bc99919f17
aariz0012 <185970087+aariz0012@users.noreply.github.com>
2025-07-29 12:33:52 +0530

    Update Navbar.js

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -88,7 +88,7 @@ const Navbar = () => {
                 )}
               </div>
               <Link href="/contact" className={navLinkClasses('/contact')}>
-                Contact
+                Contact Us
               </Link>
             </div>
           </div>
@@ -220,7 +220,7 @@ const Navbar = () => {
             <Link
               href="/contact"
               className={`${router.pathname === '/contact' ? 'bg-primary-50 border-primary-500 text-primary-700' : 'border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700'} block pl-3 pr-4 py-2 border-l-4 text-base font-medium`}>
-              Contact
+              Contact Us
             </Link>
           </div>
         </div>
