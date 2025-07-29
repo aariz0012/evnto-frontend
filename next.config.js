5a759ad44d9af52620ef6ea9e3766c7a1bfbaf76
aariz0012 <185970087+aariz0012@users.noreply.github.com>
2025-07-29 15:39:54 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['localhost', 'enjoy-booking-api.onrender.com'],
     unoptimized: true,
   },
-
+  webpack: (config) => {
+    // Add path aliases
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
 }
 
 module.exports = nextConfig;
