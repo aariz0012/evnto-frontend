b051d94f437b736561f872be98126f53c1100a7a
aariz0012 <185970087+aariz0012@users.noreply.github.com>
2025-07-30 14:33:41 +0530

    Add files via upload

---
+++ src/pages/_document.js
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
