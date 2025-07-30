316739120399249d2e90781755e8003b39b3d102
aariz0012 <185970087+aariz0012@users.noreply.github.com>
2025-07-30 15:22:20 +0530

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,12 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta charSet="utf-8" />
-        <meta name="theme-color" content="#000000" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <meta name="description" content="Venuity - Book your favorite venues and experiences" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <body>
         <Main />
