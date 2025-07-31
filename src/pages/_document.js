a881b101ac1720158115c50042ee26f903a3197f
aariz0012 <185970087+aariz0012@users.noreply.github.com>
2025-07-31 14:37:37 +0530

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,9 +5,24 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta charSet="utf-8" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="Venuity - Book your favorite venues and experiences" />
+        
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        
+        {/* PWA Support */}
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        
+        {/* Preconnect to important origins */}
+        <link rel="preconnect" href="https://evento-backend-77q2.onrender.com" />
+        <link rel="preconnect" href="https://js.stripe.com" crossOrigin="anonymous" />
       </Head>
-      <body>
+      <body className="antialiased">
         <Main />
         <NextScript />
       </body>
