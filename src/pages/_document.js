7b69a450e70208a8dabaf9368ee956211c94d68b
aariz0012 <185970087+aariz0012@users.noreply.github.com>
2025-08-02 14:45:03 +0530

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,6 +7,10 @@ export default function Document() {
         <meta charSet="utf-8" />
         <meta name="theme-color" content="#000000" />
         <meta name="description" content="Venuity - Book your favorite venues and experiences" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        
+        {/* Polyfills for older browsers */}
+        <script src="https://polyfill.io/v3/polyfill.min.js?features=default%2Ces2015%2Ces2016%2Ces2017%2Ces2018%2Ces2019%2Ces2020%2Ces2021%2Ces2022%2Cfetch%2CIntersectionObserver%2CResizeObserver%2CWebAnimations" crossOrigin="anonymous" />
         
         {/* Favicon */}
         <link rel="icon" href="/favicon.ico" sizes="any" />
@@ -17,12 +21,29 @@ export default function Document() {
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        
-        {/* Preconnect to important origins */}
-        <link rel="preconnect" href="https://evento-backend-77q2.onrender.com" />
-        <link rel="preconnect" href="https://js.stripe.com" crossOrigin="anonymous" />
       </Head>
-      <body className="antialiased">
+      <body>
+        <noscript>
+          <div style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: 'white',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: '20px',
+            textAlign: 'center',
+            zIndex: 9999
+          }}>
+            <div>
+              <h2>JavaScript is required</h2>
+              <p>Please enable JavaScript to run this application.</p>
+            </div>
+          </div>
+        </noscript>
         <Main />
         <NextScript />
       </body>
